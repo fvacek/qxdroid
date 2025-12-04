698c4627780f0b941014031a329024c2bda032d3
Fanda Vacek <fvacek@elektroline.cz>
2025-12-04 22:03:29 +0100

    Add more chainpack tests

--- app/src/test/java/org/qxqx/qxdroid/shv/ChainPackTest.kt
+++ app/src/test/java/org/qxqx/qxdroid/shv/ChainPackTest.kt
@@ -146,4 +146,57 @@ class ChainPackTest {
         ))
         testReadWriteTwist(list, "8886016182807BFE88414243FF80FF")
     }
+
+    @Test
+    fun testMap() {
+        val map = RpcValue.Map(mapOf(
+            "bar" to RpcValue.Int(2),
+            "baz" to RpcValue.Int(3),
+            "foo" to RpcValue.List(listOf(RpcValue.Int(11), RpcValue.Int(12), RpcValue.Int(13)))
+        ))
+        testReadWriteTwist(map, "89860362617242860362617A438603666F6F884B4C4DFFFF")
+    }
+
+    @Test
+    fun testIMap() {
+        val map = RpcValue.IMap(mapOf(
+            1 to RpcValue.String("foo"),
+            2 to RpcValue.String("bar"),
+            333 to RpcValue.Int(15),
+        ))
+        testReadWriteTwist(map, "8A418603666F6F42860362617282814D4FFF")
+    }
+
+    @Test
+    fun testDouble() {
+        testReadWriteTwist(RpcValue.Double(-9.094_583_978_896_067E-39), "830000000000C208B8")
+    }
+
+    @Test
+    fun testDecimal() {
+        testReadWriteTwist(RpcValue.Decimal(0, 0), "8C0000")
+        testReadWriteTwist(RpcValue.Decimal(321, -2), "8C814142")
+    }
+
+    @Test
+    fun testDateTime() {
+        testReadWriteTwist(RpcValue.DateTime(1517529600001, 0), "8D04");
+        testReadWriteTwist(RpcValue.DateTime(1517529600001, 3600), "8D8211");
+        testReadWriteTwist(RpcValue.DateTime(1543708800000, 0), "8DE63DDA02");
+        testReadWriteTwist(RpcValue.DateTime(1514764800000, 0), "8DE8A8BFFE");
+        testReadWriteTwist(RpcValue.DateTime(1546300800000, 0), "8DE6DC0E02");
+        testReadWriteTwist(RpcValue.DateTime(1577836800000, 0), "8DF00E60DC02");
+        testReadWriteTwist(RpcValue.DateTime(1609459200000, 0), "8DF015EAF002");
+        testReadWriteTwist(RpcValue.DateTime(1924992000000, 0), "8DF061258802");
+        testReadWriteTwist(RpcValue.DateTime(2240611200000, 0), "8DF100AC656602");
+        testReadWriteTwist(RpcValue.DateTime(2246004900000, -36900), "8DF156D74D495F");
+        testReadWriteTwist(RpcValue.DateTime(2246004900123, -36900), "8DF301533905E2375D");
+        testReadWriteTwist(RpcValue.DateTime(0, 0), "8DF18169CEA7FE");
+        testReadWriteTwist(RpcValue.DateTime(1493790723000, 0), "8DEDA8E7F2");
+        testReadWriteTwist(RpcValue.DateTime(1493826723923, 0), "8DF1961334BEB4");
+        testReadWriteTwist(RpcValue.DateTime(1493790751123, 36000), "8DF28B0DE42CD95F");
+        testReadWriteTwist(RpcValue.DateTime(1493826723000, 0), "8DEDA6B572");
+        testReadWriteTwist(RpcValue.DateTime(1493832123000, -5400), "8DF182D3308815");
+        testReadWriteTwist(RpcValue.DateTime(1493826723923, 0), "8DF1961334BEB4");
+    }
 }
