2fb9160080bf09516dd72d1a99c62eda67ef29b1
Fanda Vacek <fanda.vacek@gmail.com>
2026-08-25 22:12:55 +0200

    Bump app version to 1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.qxqx.qxdroid"
         minSdk = 33
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
